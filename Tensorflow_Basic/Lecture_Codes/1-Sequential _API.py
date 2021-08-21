5b43df1d178c981e36a8ee016014f246bebad5b5
amir-jafari <amir.h.jafari@okstate.edu>
2021-08-21 02:01:19 -0400

    Updated

--- Tensorflow_Basic/Lecture_Codes/1-Sequential _API.py
+++ Tensorflow_Basic/Lecture_Codes/1-Sequential _API.py
@@ -1,11 +1,11 @@
 # # %%---------------------------------------------------------------------------------------------
-import keras
+import tensorflow
 from tensorflow.keras import layers
 import numpy as np
 x = np.linspace(-4,4,500)
 y = np.sin(x)
 
-model = keras.Sequential()
+model = tensorflow.Sequential()
 
 model.add(layers.Dense(10, activation='relu' , input_shape=(1,)))
 model.add(layers.Dense(1, activation='linear' , input_shape=(10,)))
--- Tensorflow_Basic/Lecture_Codes/2-Functional _API.py
+++ Tensorflow_Basic/Lecture_Codes/2-Functional _API.py
@@ -1,14 +1,14 @@
 # # %%---------------------------------------------------------------------------------------------
-import keras
+import tensorflow
 from tensorflow.keras import layers
 import numpy as np
 x = np.linspace(-4,4,500)
 y = np.sin(x)
 
-inputs = keras.Input(shape=(1,))
+inputs = tensorflow.keras.Input(shape=(1,))
 x1 =layers.Dense(10,activation='relu')(inputs)
 ouputs = layers.Dense(1, activation='relu')(x1)
 
-model = keras.Model(inputs, ouputs)
+model = tensorflow.keras.Model(inputs, ouputs)
 model.compile(loss='mse',optimizer='sgd',metrics=['accuracy'])
 model.fit(x, y, epochs=10)
