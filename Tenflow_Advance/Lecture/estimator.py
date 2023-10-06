a510a15cb67d252514cb8f8ade8a23988f21187f
amir-jafari <amir.h.jafari@okstate.edu>
2023-10-05 21:24:57 -0400

    Update estimator.py

--- Tenflow_Advance/Lecture/estimator.py
+++ Tenflow_Advance/Lecture/estimator.py
@@ -1,16 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-import os
-import sys
-
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
-from six.moves import urllib
-
-import tensorflow.compat.v2.feature_column as fc
-
 import tensorflow as tf
 
 # Load dataset.
