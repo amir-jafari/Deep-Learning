9d1426853a15d7c30fab3bcbd7db791b78c29e01
amir-jafari <amir.h.jafari@okstate.edu>
2023-10-10 17:10:05 -0400

    Tensorflow code updates

--- Tensorflow_Basic/Lecture_Codes/1-Sequential _API.py
+++ Tensorflow_Basic/Lecture_Codes/1-Sequential _API.py
@@ -1,14 +1,13 @@
 # # %%---------------------------------------------------------------------------------------------
-import tensorflow
-from tensorflow.keras import layers
+import tensorflow as tf
 import numpy as np
 x = np.linspace(-4,4,500)
 y = np.sin(x)
 
-model = tensorflow.keras.Sequential()
+model = tf.keras.models.Sequential()
 
-model.add(layers.Dense(10, activation='relu' , input_shape=(1,)))
-model.add(layers.Dense(1, activation='linear' , input_shape=(10,)))
+model.add(tf.keras.layers.Dense(10, activation='relu' , input_shape=(1,)))
+model.add(tf.keras.layers.Dense(1, activation='linear' , input_shape=(10,)))
 
 model.compile(loss='mse',optimizer='sgd',metrics=['accuracy'])
 model.fit(x, y, epochs=10)
