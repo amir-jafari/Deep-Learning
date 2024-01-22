2a4422cef8aa93ab93e18c84532ea8bf24e71aa0
amir-jafari <amir.h.jafari@okstate.edu>
2024-01-22 17:50:48 -0500

    make changes

--- Tenflow_Advance/Lecture/estimator.py
+++ Tenflow_Advance/Lecture/estimator.py
@@ -1,8 +1,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
