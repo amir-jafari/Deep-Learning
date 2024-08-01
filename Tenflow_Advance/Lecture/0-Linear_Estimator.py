9e104a0d07ac6941213c9203cc5ed991b6424a5a
Amir <amir.h.jafari@okstate.edu>
2024-07-31 23:48:04 -0400

    Refactor to use Keras for model creation and training
    Switched from pandas-based data loading and feature engineering to TensorFlow's Keras API. Simplified data handling by converting datasets into tensors and directly fitting, compiling, and evaluating the model using Keras methods.

--- Tenflow_Advance/Lecture/0-Linear_Estimator.py
+++ Tenflow_Advance/Lecture/0-Linear_Estimator.py
@@ -1,82 +1,21 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import pandas as pd
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
 import tensorflow as tf
 
-# Load dataset.
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')
 
-print(dftrain.head())
+features = [[1., 1.5], [2., 2.5], [3., 3.5]]
+labels = [[0.3], [0.5], [0.7]]
+eval_features = [[4., 4.5], [5., 5.5], [6., 6.5]]
+eval_labels = [[0.8], [0.9], [1.]]
 
-print(dftrain.describe())
-print(dftrain.shape[0], dfeval.shape[0])
+dataset = tf.data.Dataset.from_tensor_slices((features, labels)).batch(1)
+eval_dataset = tf.data.Dataset.from_tensor_slices(
+      (eval_features, eval_labels)).batch(1)
 
-dftrain.age.hist(bins=20)
-plt.show()
+model = tf.keras.models.Sequential([tf.keras.layers.Dense(1)])
+optimizer = tf.keras.optimizers.Adagrad(learning_rate=0.05)
 
-dftrain.sex.value_counts().plot(kind='barh')
-plt.show()
+model.compile(optimizer=optimizer, loss="mse")
 
-dftrain['class'].value_counts().plot(kind='barh')
-plt.show()
-
-pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
-
-# Feature Engineering for the Model
-# Estimators use a system called feature columns to describe
-# how the model should interpret each of the raw input features.
-# An Estimator expects a vector of numeric inputs, and feature
-# columns describe how the model should convert each feature.
-
-CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
-                       'embark_town', 'alone']
-NUMERIC_COLUMNS = ['age', 'fare']
-
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()
-  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
-
-for feature_name in NUMERIC_COLUMNS:
-  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+model.fit(dataset)
 
 
-def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-  def input_function():
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
-    if shuffle:
-      ds = ds.shuffle(1000)
-    ds = ds.batch(batch_size).repeat(num_epochs)
-    return ds
-  return input_function
-
-train_input_fn = make_input_fn(dftrain, y_train)
-eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
-
-ds = make_input_fn(dftrain, y_train, batch_size=10)()
-for feature_batch, label_batch in ds.take(1):
-  print('Some feature keys:', list(feature_batch.keys()))
-  print()
-  print('A batch of class:', feature_batch['class'].numpy())
-  print()
-  print('A batch of Labels:', label_batch.numpy())
-
-age_column = feature_columns[7]
-tf.keras.layers.DenseFeatures([age_column])(feature_batch).numpy()
-
-gender_column = feature_columns[0]
-tf.keras.layers.DenseFeatures([tf.feature_column.indicator_column(gender_column)])(feature_batch).numpy()
-
-
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
-linear_est.train(train_input_fn)
-result = linear_est.evaluate(eval_input_fn)
-
-clear_output()
-print(result)
-
-age_x_gender = tf.feature_column.crossed_column(['age', 'sex'], hash_bucket_size=100)
+model.evaluate(eval_dataset, return_dict=True)
