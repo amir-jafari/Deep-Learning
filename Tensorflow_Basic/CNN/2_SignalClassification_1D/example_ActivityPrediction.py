67316ea1d97e29f096ba086bd255fa966c8da568
Amir <amir.h.jafari@okstate.edu>
2024-08-01 01:38:35 -0400

    Remove exercise_ActivityPrediction_solution.py
    Deleted the outdated solution file for 1D signal classification exercise. Updated example_ActivityPrediction.py to include necessary dataset checks, data processing, and training procedures, ensuring it now covers all required steps previously found in the deleted solution file.

--- Tensorflow_Basic/CNN/2_SignalClassification_1D/example_ActivityPrediction.py
+++ Tensorflow_Basic/CNN/2_SignalClassification_1D/example_ActivityPrediction.py
@@ -1,6 +1,3 @@
-# Download WISDM_ar_latest.tar.gz clicking on "Download Latest Version" at http://www.cis.fordham.edu/wisdm/dataset.php
-# Unzip it in the current working directory
-
 # %% --------------------------------------- Imports -------------------------------------------------------------------
 import os
 import random
@@ -15,6 +12,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import confusion_matrix
 
+# Check if dataset is already downloaded and extract it
 if "WISDM_ar_v1.1" not in os.listdir(os.getcwd()):
     try:
         os.system("wget http://www.cis.fordham.edu/wisdm/includes/datasets/latest/WISDM_ar_latest.tar.gz")
@@ -22,7 +20,6 @@ if "WISDM_ar_v1.1" not in os.listdir(os.getcwd()):
     except:
         print("There was a problem downloading the data!!")
         raise
-        # Go to http://www.cis.fordham.edu/wisdm/dataset.php and click Download Latest version, and untar to current dir
     if "WISDM_ar_v1.1" not in os.listdir(os.getcwd()):
         print("There was a problem downloading the data!!")
         import sys
@@ -40,13 +37,14 @@ LR = 1e-4
 N_EPOCHS = 15
 BATCH_SIZE = 128
 DROPOUT = 0.5
-DATA_PATH = os.getcwd() + "/WISDM_ar_v1.1/WISDM_ar_v1.1_raw.txt"
+DATA_PATH = os.path.join(os.getcwd(), "WISDM_ar_v1.1", "WISDM_ar_v1.1_raw.txt")
 
 # %% -------------------------------------- Data Prep ------------------------------------------------------------------
-data_raw = pd.read_csv(DATA_PATH, error_bad_lines=False, header=None)
+data_raw = pd.read_csv(DATA_PATH, on_bad_lines='skip', header=None)
 data_raw.dropna(inplace=True)  # There is only one NaN
 data_raw[5] = data_raw[5].apply(lambda f: float(f[:-1]))  # Fixes last column (z-axis)
 groups = data_raw.groupby(1)  # Groups by type of activity (classes)
+
 # Loops over the data to get signals per class
 x, y, min_signal_length = [], [], 10000000
 for label, group in groups:
@@ -55,46 +53,50 @@ for label, group in groups:
         y.append(label)
         if min_signal_length > len(x[-1]):
             min_signal_length = len(x[-1])
+
 # Uses the min_signal_length to get fixed length signals, but this means losing some data
 x_prep, y_prep = [], []
 for signal, label in zip(x, y):
     for i in range(len(signal)//min_signal_length):  # Losing some data here
         x_prep.append(signal[min_signal_length*i:min_signal_length*(i+1)])
         y_prep.append(label)
+
 x, y = np.array(x_prep), np.array(y_prep)
 del x_prep, y_prep
 print(np.unique(y, return_counts=True))
+
 # Final usual pre-processing
 le = LabelEncoder()
 y = le.fit_transform(y)
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=SEED, test_size=0.2, stratify=y)
 sd = StandardScaler()
+x_train = sd.fit_transform(x_train.reshape(-1, x_train.shape[-1])).reshape(x_train.shape)
+x_test = sd.transform(x_test.reshape(-1, x_test.shape[-1])).reshape(x_test.shape)
 y_train, y_test = to_categorical(y_train, num_classes=6), to_categorical(y_test, num_classes=6)
-# x_train and x_test are already on the suitable shape (batch, steps, channels)
 
 # %% -------------------------------------- Training Prep ----------------------------------------------------------
-model = Sequential([  # min_signal_length turns out to be 544, so we pick 4 1D-convs of 544/4 kernel size each
-    Conv1D(16, 136, activation="relu"),  # output (batch, 409, 16)
+model = Sequential([
+    Conv1D(16, 136, activation="relu", input_shape=(x_train.shape[1], x_train.shape[2])),  # Adjust input shape
     BatchNormalization(),
-    Conv1D(32, 136, activation="relu"),  # output (batch, 274, 32)
+    Conv1D(32, 136, activation="relu"),
     BatchNormalization(),
-    Conv1D(64, 136, activation="relu"),  # output (batch, 139, 64)
+    Conv1D(64, 136, activation="relu"),
     BatchNormalization(),
-    Conv1D(128, 136, activation="relu"),  # output (batch, 4, 128)
+    Conv1D(128, 136, activation="relu"),
     BatchNormalization(),
-    Flatten(),  # output (batch, 512)
-    Dense(136, activation="relu"),  # Intermediate linear layer that comes back to the kernel size
+    Flatten(),
+    Dense(136, activation="relu"),
     Dropout(DROPOUT),
     BatchNormalization(),
     Dense(6, activation="softmax")
 ])
-model.compile(optimizer=Adam(lr=LR), loss="categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer=Adam(learning_rate=LR), loss="categorical_crossentropy", metrics=["accuracy"])
 
 # %% -------------------------------------- Training Loop ----------------------------------------------------------
 model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=N_EPOCHS, validation_data=(x_test, y_test))
 
 # %% ------------------------------------------ Final test -------------------------------------------------------------
-print("Final accuracy on validations set:", 100*model.evaluate(x_test, y_test)[1], "%")
+print("Final accuracy on validation set:", 100*model.evaluate(x_test, y_test)[1], "%")
 y_test_pred = np.argmax(model.predict(x_test), axis=1)
-print(le.inverse_transform(np.array([0, 1, 2, 3, 4, 5])))
+print(le.inverse_transform(np.arange(6)))
 print(confusion_matrix(np.argmax(y_test, axis=1), y_test_pred))
